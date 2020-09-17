c0272529dad2900e30a4e36d04dcd5492d6ac7b4
Brandon McGuire <brandonmcguire1992@gmail.com>
2020-09-17 18:19:07 -0500

    add validation

--- models/Pizza.js
+++ models/Pizza.js
@@ -4,18 +4,24 @@ const moment = require('moment');
 const PizzaSchema = new Schema(
 	{
 		pizzaName : {
-			type : String
+			type : String,
+			required: true,
+			trim: true
 		},
 		createdBy : {
-			type : String
+			type : String,
+			required: true,
+			trim: true
 		},
 		createdAt : {
 			type    : Date,
-      default : Date.now,
-      get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+      		default : Date.now,
+      		get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
 		},
 		size      : {
 			type    : String,
+			required: true,
+			enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
 			default : 'Large'
 		},
 		toppings  : [],
